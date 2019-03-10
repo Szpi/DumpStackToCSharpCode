2b4b25e20af346f97fe1cdac2af734e7006cd830
Tisu <piotrszkudlarski@gmail.com>
2019-03-10 20:09:05 +0100

    Changed duplicated test to test member that is interface

--- DumpStackToCSharpCode/DumpStackToCSharpCodeTests/ObjectInitializationGeneration/GenerateComplexObjectTypeTests.cs
+++ DumpStackToCSharpCode/DumpStackToCSharpCodeTests/ObjectInitializationGeneration/GenerateComplexObjectTypeTests.cs
@@ -63,22 +63,12 @@ namespace DumpStackToCSharpCodeTests.ObjectInitializationGeneration
         [Test]
         public void ShouldGenerate_ComplexObjectAssignment_WithInterface()
         {
-            var firstElement = new ExpressionData("string", "test1", "[0]", new ExpressionData[] { }, "string");
-            var secondElement = new ExpressionData("string", "test2", "[1]", new ExpressionData[] { }, "string");
-            var array = new ExpressionData("string[]",
-                                           "{string[2]}",
-                                           "testStringArray",
-                                           new[]
-                                           {
-                                               firstElement,
-                                               secondElement
-                                           },
-                                           "string[]");
+            var firstElement = new ExpressionData("test", "{ConsoleApp1.test}", "TestTestTest", new ExpressionData[] { }, "ConsoleApp1.Itest {ConsoleApp1.test}");
 
-            var stackObject = new ExpressionData("Test", "Test", "testComplexObject", new [] { array }, "Test");
+            var stackObject = new ExpressionData("Test", "Test", "testComplexObject", new [] { firstElement }, "Test");
             var generated = _codeGeneratorManager.GenerateStackDump(stackObject);
 
-            generated.Should().Be("var testComplexObject = new Test()\n{\r\n    testStringArray = new string[] { \"test1\", \"test2\" }\r\n};\n");
+           generated.Should().Be("var testComplexObject = new Test()\n{\r\n    TestTestTest = new test()\r\n    {\r\n    }\r\n};\n");
         }
     }
 }
