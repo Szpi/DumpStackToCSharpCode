7d852b940318bc63a5aaa8605bba7344a6629c42
Tisu <piotrszkudlarski@gmail.com>
2020-02-17 23:55:25 +0100

    Fix generating string when " is in string, fixed readonlytypetests

--- DumpStackToCSharpCode/DumpStackToCSharpCode/ObjectInitializationGeneration/Expression/PrimitiveExpressionGenerator.cs
+++ DumpStackToCSharpCode/DumpStackToCSharpCode/ObjectInitializationGeneration/Expression/PrimitiveExpressionGenerator.cs
@@ -27,65 +27,65 @@ namespace RuntimeTestDataCollector.ObjectInitializationGeneration.Expression
                 case TypeCode.Byte:
                 case TypeCode.SByte:
                 case TypeCode.Int:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(int.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(int.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.Float:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(float.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(float.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.Double:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(double.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(double.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.Decimal:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(decimal.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(decimal.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.UInt:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(uint.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(uint.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.Long:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(long.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(long.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.ULong:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.NumericLiteralExpression,
-                        SyntaxFactory.Literal(ulong.Parse(value, Thread.CurrentThread.CurrentUICulture)));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.NumericLiteralExpression,
+                            SyntaxFactory.Literal(ulong.Parse(value, Thread.CurrentThread.CurrentUICulture)));
+                    }
                 case TypeCode.Char:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.CharacterLiteralExpression,
-                        SyntaxFactory.Literal(value.Trim('\"'),'\''));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.CharacterLiteralExpression,
+                            SyntaxFactory.Literal(value.Trim('\"'), '\''));
+                    }
                 case TypeCode.String:
-                {
-                    return SyntaxFactory.LiteralExpression(
-                        SyntaxKind.StringLiteralExpression,
-                        SyntaxFactory.Literal(value.Trim('\"')));
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(
+                            SyntaxKind.StringLiteralExpression,
+                            SyntaxFactory.Literal(value.Replace("\\", string.Empty).Trim('\"')));
+                    }
                 case TypeCode.Boolean:
-                {
-                    return SyntaxFactory.LiteralExpression(value == TrueValue
-                                                               ? SyntaxKind.TrueLiteralExpression
-                                                               : SyntaxKind.FalseLiteralExpression);
-                }
+                    {
+                        return SyntaxFactory.LiteralExpression(value == TrueValue
+                                                                   ? SyntaxKind.TrueLiteralExpression
+                                                                   : SyntaxKind.FalseLiteralExpression);
+                    }
                 default:
                     throw new NotImplementedException();
             }
--- DumpStackToCSharpCode/DumpStackToCSharpCodeTests/ObjectInitializationGeneration/GenerateReadonlyTypeTests.cs
+++ DumpStackToCSharpCode/DumpStackToCSharpCodeTests/ObjectInitializationGeneration/GenerateReadonlyTypeTests.cs
@@ -28,70 +28,62 @@ namespace DumpStackToCSharpCodeTests.ObjectInitializationGeneration
         {
             var stackObject = new ExpressionData("ExpressionData", "{ConsoleApp1.Program.ExpressionData}", "mainObject", new List<ExpressionData>()
             {
-                new ExpressionData("string", "\\\"testList\\", "Name", new List<ExpressionData>()
+                new ExpressionData("string", "testList", "Name", new List<ExpressionData>()
                 {
                 }, "string"),
-                new ExpressionData("string", "\\\"List<ExpressionData>\\", "Type", new List<ExpressionData>()
+                new ExpressionData("string", "List<ExpressionData>", "Type", new List<ExpressionData>()
                 {
                 }, "string"),
-                new ExpressionData("string", "\\\"System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>\\", "TypeWithNamespace", new List<ExpressionData>()
+                new ExpressionData("string", "System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>", "TypeWithNamespace", new List<ExpressionData>()
                 {
                 }, "string"),
                 new ExpressionData("List<ExpressionData>", "Count = 1", "UnderlyingExpressionData", new List<ExpressionData>()
                 {
                     new ExpressionData("ExpressionData", "{ConsoleApp1.Program.ExpressionData}", "[0]", new List<ExpressionData>()
                     {
-                        new ExpressionData("string", "\\\"[0]\\", "Name", new List<ExpressionData>()
+                        new ExpressionData("string", "[0]", "Name", new List<ExpressionData>()
                         {
                         }, "string"),
-                        new ExpressionData("string", "\\\"ExpressionData\\", "Type", new List<ExpressionData>()
+                        new ExpressionData("string", "ExpressionData", "Type", new List<ExpressionData>()
                         {
                         }, "string"),
-                        new ExpressionData("string", "\\\"ConsoleApp1.Program.ExpressionData\\", "TypeWithNamespace", new List<ExpressionData>()
+                        new ExpressionData("string", "ConsoleApp1.Program.ExpressionData", "TypeWithNamespace", new List<ExpressionData>()
                         {
                         }, "string"),
                         new ExpressionData("List<ExpressionData>", "Count = 1", "UnderlyingExpressionData", new List<ExpressionData>()
                         {
                             new ExpressionData("ExpressionData", "{ConsoleApp1.Program.ExpressionData}", "[0]", new List<ExpressionData>()
                             {
-                                new ExpressionData("string", "\\\"Name\\", "Name", new List<ExpressionData>()
+                                new ExpressionData("string", "Name", "Name", new List<ExpressionData>()
                                 {
                                 }, "string"),
-                                new ExpressionData("string", "\\\"string\\", "Type", new List<ExpressionData>()
+                                new ExpressionData("string", "string", "Type", new List<ExpressionData>()
                                 {
                                 }, "string"),
-                                new ExpressionData("string", "\\\"string\\", "TypeWithNamespace", new List<ExpressionData>()
+                                new ExpressionData("string", "string", "TypeWithNamespace", new List<ExpressionData>()
                                 {
                                 }, "string"),
                                 new ExpressionData("IReadOnlyList<ExpressionData>", "null", "UnderlyingExpressionData", new List<ExpressionData>()
                                 {
                                 }, "System.Collections.Generic.IReadOnlyList<ConsoleApp1.Program.ExpressionData>"),
-                                new ExpressionData("string", "\\\"ExpressionData21212\\", "Value", new List<ExpressionData>()
+                                new ExpressionData("string", "ExpressionData21212", "Value", new List<ExpressionData>()
                                 {
                                 }, "string")
                             }, "ConsoleApp1.Program.ExpressionData")
                         }, "System.Collections.Generic.IReadOnlyList<ConsoleApp1.Program.ExpressionData> {System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>}"),
-                        new ExpressionData("string", "\\\"{ConsoleApp1.Program.ExpressionData}\\", "Value", new List<ExpressionData>()
+                        new ExpressionData("string", "{ConsoleApp1.Program.ExpressionData}", "Value", new List<ExpressionData>()
                         {
                         }, "string")
                     }, "ConsoleApp1.Program.ExpressionData")
                 }, "System.Collections.Generic.IReadOnlyList<ConsoleApp1.Program.ExpressionData> {System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>}"),
-                new ExpressionData("string", "\\\"Count = 1\\", "Value", new List<ExpressionData>()
+                new ExpressionData("string", "Count = 1", "Value", new List<ExpressionData>()
                 {
                 }, "string")
             }, "ConsoleApp1.Program.ExpressionData");
-
-
-
+                                 
             var generated = _codeGeneratorManager.GenerateStackDump(stackObject);
 
-            generated.Should().Be(@"var mainObject = new ExpressionData(""List<ExpressionData>"", ""Count = 1"", ""testList"", new List<ExpressionData>()
-                                {
-                                    new ExpressionData(""ExpressionData"", ""{ConsoleApp1.Program.ExpressionData}"", ""[0]"", new List<ExpressionData>()
-                                    {
-                                        new ExpressionData(""string"", ""ExpressionData21212"", ""Name"", null, ""string"")
-                                    }, ""ConsoleApp1.Program.ExpressionData"")
-                                }, ""System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>"");");
+            generated.Should().Be("var mainObject = new ExpressionData(\"List<ExpressionData>\", \"Count = 1\", \"testList\", new List<ExpressionData>()\n{\r\n    new ExpressionData(\"ExpressionData\", \"{ConsoleApp1.Program.ExpressionData}\", \"[0]\", new List<ExpressionData>()\r\n    {\r\n        new ExpressionData(\"string\", \"ExpressionData21212\", \"Name\", null, \"string\")\r\n    }, \"ConsoleApp1.Program.ExpressionData\")\r\n}, \"System.Collections.Generic.List<ConsoleApp1.Program.ExpressionData>\");\n");
         }
     }
 }
